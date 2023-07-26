77b2d2652cb3f5ccdf157f08b7d7c05e4fc849aa
GyanendraMohan <gmpatel152@gmail.com>
2023-07-26 10:41:42 +0530

    moragn setup

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+//express
+const express = require("express");
+const app = express();
+
+//rest of the packages
+const morgan = require("morgan");
+
+//database
+const connectDB = require("./db/connect");
+
+//middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("E-Commerce API");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening at ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
